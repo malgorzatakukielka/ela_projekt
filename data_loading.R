a8f6f331f7ea6f263895a163247283e7243995e9
Małgorzata Kukiełka <mk304979@student.polsl.pl>
2024-11-25 18:33:32 +0100

    data loading 25.11

--- data_loading.R
+++ data_loading.R
@@ -1,9 +1,11 @@
 #ładowanie pakietów
 library(tidyverse)
 
-ela <- read.csv2("~/ela_projekt/graduates-major-data(1).csv")
+ela_nowe <- read.csv2("~/ela_projekt/graduates-major-data-nowe.csv")
 
-ela1 <- select(ela, P_NAZWA_UCZELNI, P_KIERUNEK_NAZWA, P_POZIOM, P_FORMA, P_ME_ZAR_P1, P_ME_ZAR_P2, P_ROKDYP) %>% 
+ela1 <- select(ela_nowe, P_NAZWA_UCZELNI, P_KIERUNEK_NAZWA, P_POZIOM, P_FORMA, 
+               P_ME_ZAR_P1, P_ME_ZAR_P2, P_ROKDYP, P_NAZWA_JEDN, 
+               P_KIERUNEK_ID, P_NAZWA_KIERUNKU_PELNA, P_N) %>% 
   pivot_longer(cols = 5:6, names_to = "p", values_to = "me_zar") #wybór kolumn
 
 ela1$p <- factor(ela1$p, levels = c("P_ME_ZAR_P1", "P_ME_ZAR_P2"), 
@@ -17,9 +19,11 @@ ela1$P_POZIOM <- factor(ela1$P_POZIOM, levels = c("1", "2", "JM"),
                                    "Studia jednolite magisterskie"))
 
 ela1$P_POZIOMFORMA <- paste(ela1$P_POZIOM, ela1$P_FORMA, sep = ", ") 
-#dodanie noej kolumny, zespolenie dotychcasowych kolumn P_POZIOM i P_FORMA
+#dodanie nowej kolumny, zespolenie dotychcasowych kolumn P_POZIOM i P_FORMA
 
-ela1 <- ela1[, c(1, 2, 8, 5, 6, 7)] #usunięcie kolumn forma i poziom
+ela1 <- select(ela1, P_NAZWA_UCZELNI, P_KIERUNEK_NAZWA, P_POZIOMFORMA, 
+               P_ROKDYP, P_NAZWA_JEDN, 
+               P_NAZWA_KIERUNKU_PELNA, p, me_zar, P_N, P_KIERUNEK_ID)
 
 ela1 <- arrange(ela1, P_NAZWA_UCZELNI, P_KIERUNEK_NAZWA) 
 #ułożenie alfabetycznie wg uczelni i kierunku
